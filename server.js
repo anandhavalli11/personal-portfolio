2bff5a4995b8c836be99ac0430877bb3d2e9863a
anandhavalli11 <anandhavalli1142007@gmail.com>
2026-06-07 10:13:49 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+app.get("/",(req, res) => {
+    res.send("Portfolio Backend is Running Successfully!");
+});
+app.listen(PORT,() =>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
